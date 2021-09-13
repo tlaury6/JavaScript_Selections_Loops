1afa567a78104868d557f78193da30678c6389ce
Taylor Laury <tlaury6@yahoo.com>
2021-09-13 14:28:57 -0400

    Completed JS Loops Exercise

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-
+for (let i = 1; i <= 100; i++){
+    if (i % 2 == 1) {
+        console.log(i)
+    }else{
+        continue; 
+    }
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (i = 1; i <= 100 ; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log ("FIZZBUZZ");
+    } else  if (i % 3 == 0 ){
+        console.log ("FIZZ");
+    }else if (i % 5 == 0){
+        console.log("BUZZ");
+    }else {
+        console.log(i);
+    }
+}
